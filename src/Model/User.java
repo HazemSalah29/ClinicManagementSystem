0bae6a72bd60c21d66b239ef8fa12bee222baf4f
hazem salah <hazem.salah29@gmail.com>
2021-01-15 21:18:59 +0200

    this a clinic management system

--- src/Model/User.java
+++ src/Model/User.java
@@ -13,8 +13,68 @@ import Model.Login;
 public class User {
     private String UserName;
     private int UserId;
+    private String UserType;
     private int UserPass;
     private String email;
+
+    public void setUserType(String UserType) {
+        this.UserType = UserType;
+    }
+
+    public void setUserName(String UserName) {
+        this.UserName = UserName;
+    }
+
+    public void setUserId(int UserId) {
+        this.UserId = UserId;
+    }
+
+    public void setUserPass(int UserPass) {
+        this.UserPass = UserPass;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setLogin(Login login) {
+        this.login = login;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public int getUserId() {
+        return UserId;
+    }
+
+    public int getUserPass() {
+        return UserPass;
+    }
+
+    public String getUserType() {
+        return UserType;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Login getLogin() {
+        return login;
+    }
+
+    public User(String UserName, int UserId, String UserType, int UserPass, String email, Login login) {
+        this.UserName = UserName;
+        this.UserId = UserId;
+        this.UserType = UserType;
+        this.UserPass = UserPass;
+        this.email = email;
+        this.login = login;
+    }
+
+    
     
     public Login login;
 
